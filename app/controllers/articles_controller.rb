e2bbfaaf9431d93da84ceab48fac603f03fa28b3
Munir <munircittagong@yahoo.com>
2017-06-15 14:41:58 +0100

    implemented edit article with bdd

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,21 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			flash[:alert] = "Article has been updated successfully"
+			redirect_to @article
+		else
+			flash.now[:alert] = "Article could not be updated"
+			render "edit"
+		end
+	end
 
 	private
 		def article_params
